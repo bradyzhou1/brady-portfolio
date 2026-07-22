c8d8484ff9a9d0990317b581951ed805f4e45f5d
Brady Zhou <bradypzhou@gmail.com>
2026-07-21 21:42:45 -0400

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,8 @@ export default function Navbar() {
         </Link>
 
         <nav className="navLinks" aria-label="Main navigation">
-          <a href="/#about">About</a>
-          <a href="/#academic-technical">Academic</a>
+          <a href="/#about">About Me</a>
+          <a href="/#academic-technical">Academics</a>
           <a href="/#research">ISEF/STS Research</a>
           <a href="/#parentlensai">ParentLensAI</a>
           <a href="/#projects">Other Projects</a>
